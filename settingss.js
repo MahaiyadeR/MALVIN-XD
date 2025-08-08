e542b74d26a7ae9f755595f37c418c24b387d7d3
MahaiyadeR <owenermahiya@gmail.com>
2025-08-08 20:43:45 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~ZvE3AR7C#ColpB9kkmAWwcoo5oEgl-dLr7QB_pQIv_FTK6IxrJ2A",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
